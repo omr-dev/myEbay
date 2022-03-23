c9d2c8a864932ac12f1656b1c447036cc7a2e15c
omer <peynirciomerfaruk@gmail.com>
2022-03-23 15:54:13 +0100

    pages created and imported

---
+++ js/pages.js
@@ -0,0 +1,26 @@
+export const pages = [
+  {
+    id: 1,
+    idCode: "home",
+    title: "Home",
+    accessGroups: "loggedInUsers,loggedOutUsers",
+  },
+  {
+    id: 2,
+    idCode: "adDetails",
+    title: "Details",
+    accessGroups: "members,loggedOutUsers",
+  },
+  {
+    id: 3,
+    idCode: "admin",
+    title: "Admin",
+    accessGroups: "administrators",
+  },
+  {
+    id: 4,
+    idCode: "login",
+    title: "Login",
+    accessGroups: "loggedOutUsers",
+  },
+];
