9a6ab6fd6a720779024aacbe22adf8d1fa1dbb11
omer <peynirciomerfaruk@gmail.com>
2022-03-23 19:32:49 +0100

    menu manager created

--- js/app.js
+++ js/app.js
@@ -7,12 +7,13 @@ let currentUser = users.find((m) => m.login === "anonymous");
 
 //define elements
 const navItemLoggedAsElem = document.querySelector(".nav-link.loggedAs");
-const navItemHomeElem = document.querySelector(".nav-link.home");
-const navItemAddAdElem = document.querySelector(".nav-link.addAd");
-const navItemLoginElem = document.querySelector(".nav-link.login");
-const navItemRegisterElem = document.querySelector(".nav-link.register");
-const navItemAdminElem = document.querySelector(".nav-link.admin");
-const navItemDetailsElem = document.querySelector(".nav-link.details");
+const navItemHomeElem = document.querySelector(".nav-item.homePage");
+const navItemAddAdElem = document.querySelector(".nav-item.addAdPage");
+const navItemLoginElem = document.querySelector(".nav-item.loginPage");
+const navItemRegisterElem = document.querySelector(".nav-item.registerPage");
+const navItemAdminElem = document.querySelector(".nav-item.adminPage");
+const navItemDetailsElem = document.querySelector(".nav-item.adsDetailsPage");
+const navItemNodeElems = document.querySelectorAll("nav .nav-item");
 
 //set up elements
 navItemLoggedAsElem.addEventListener("click", function (e) {
@@ -55,10 +56,7 @@ const pageItems = pages.map((page) => {
 
 //general functions
 const pageManager = (idCode) => {
-  console.log("idCode", idCode);
-
   pageItems.forEach((pageItem) => {
-    console.log(pageItem);
     pageItem.elem.style.display = "none";
   });
   pageItems.find((pageItem) => pageItem.idCode === idCode).elem.style.display =
@@ -80,9 +78,42 @@ const userManager = (user) => {
   }
 };
 
+function menuManager() {
+  const navItems = Array.from(navItemNodeElems);
+
+  navItems.forEach((navItem) => {
+    const menuToPageRefId = navItem.className.split(" ")[1];
+
+    if (
+      !atLeastOneTermMatchesInLists(
+        currentUser.accessGroups,
+        getPageObj(menuToPageRefId).accessGroups
+      )
+    ) {
+      navItem.style.display = "none";
+    }
+  });
+}
+const getPageObj = (pageIdCode) => {
+  return pages.find((page) => page.idCode === pageIdCode);
+};
+
+const atLeastOneTermMatchesInLists = (list1, list2) => {
+  const list1Terms = list1.split(",");
+  const list2Terms = list2.split(",");
+  for (const list1Term of list1Terms) {
+    for (const list2Term of list2Terms) {
+      if (list1Term == list2Term) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
 //PAGE LOAD
 pageManager(currentPageIdCode);
 userManager(currentUser);
+menuManager();
 
 //////////////////////////////////////////OLD CODES
 
