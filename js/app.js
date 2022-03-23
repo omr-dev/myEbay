247fd35c1d15f8f5640cc60456fb92764dd85ecd
omer <peynirciomerfaruk@gmail.com>
2022-03-23 17:09:41 +0100

    navbar links defined and setup

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
 import { users } from "./data/users.js";
 import { pages } from "./data/pages.js";
+
+//define elements
+const navItemAddAdElem = document.querySelector(".nav-link.addAd");
+const navItemLoginElem = document.querySelector(".nav-link.login");
+const navItemRegisterElem = document.querySelector(".nav-link.register");
+
+//set up elements
+navItemAddAdElem.addEventListener("click", function (e) {
+  pageManager("addAd");
+});
+navItemLoginElem.addEventListener("click", function (e) {
+  pageManager("login");
+  //fieldLoginElem.focus();
+});
+navItemRegisterElem.addEventListener("click", function (e) {
+  pageManager("register");
+});
+
 const db = new DB("./app/dbTable.json");
 const allAdsInDb = db.getAllAdsInDB().then((data) => {
   updateUI(data);
